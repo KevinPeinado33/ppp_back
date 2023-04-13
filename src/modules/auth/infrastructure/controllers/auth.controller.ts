3eb99e724d492882f69b09ce2242dc768da54923
KevinPeinado33 <kevinpeinado@upeu.edu.pe>
2023-04-13 13:50:31 -0500

    mod controller

--- src/modules/auth/infrastructure/controllers/auth.controller.ts
+++ src/modules/auth/infrastructure/controllers/auth.controller.ts
@@ -2,15 +2,29 @@ import { Request, Response } from 'express'
 
 import { LoginUseCase } from '../../app/usecases'
 import { LoginDto } from '../../app/dtos'
+import { UserRepository } from '../../domain/repositories'
 
 export class AuthController {
+
+    private userRepository: UserRepository
  
-    constructor() { }
+    constructor() {
+
+        this.userRepository = new UserRepository()
+
+        this.postLogin    = this.postLogin.bind( this )
+        this.postRegister = this.postRegister.bind( this )
+
+    }
 
     postLogin(req: Request, res: Response) {
 
         const loginDto = req.body as LoginDto
-        const usecase  = new LoginUseCase()
+        const usecase  = new LoginUseCase(
+            res,
+            loginDto,
+            this.userRepository
+        )
 
         usecase.execute()
 
