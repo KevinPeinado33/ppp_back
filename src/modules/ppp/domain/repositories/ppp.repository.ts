706db755589011c43f35f4b0722896321bcfc0fd
Aldair-cloud <55162195+Aldair-cloud@users.noreply.github.com>
2023-07-27 23:10:42 -0500

    usando la fe en el ppp

--- src/modules/ppp/domain/repositories/ppp.repository.ts
+++ src/modules/ppp/domain/repositories/ppp.repository.ts
@@ -1,3 +1,7 @@
+import { PPPEntity } from "../../data/entities";
+
 export abstract class PPPRepository {
-    
+  abstract findOnebyId(id: string): Promise<PPPEntity | null>;
+
+  abstract save(ppp: PPPEntity): Promise<PPPEntity>;
 }
--- src/modules/ppp/presentation/controller/evaluation.controller.ts
+++ src/modules/ppp/presentation/controller/evaluation.controller.ts
@@ -35,6 +35,7 @@ export class EvaluationController {
         usecase.execute()
 
     }
+    
 
     getResultSatisfaction(req: Request, res: Response) {
 
