bac6069454383215a9ae3568ddefd4b2fb40ba49
Madai Mireli <madaichuco@hotmail.com>
2023-08-06 10:36:25 -0500

    algo de avance create-ppp-document

--- src/modules/ppp/domain/repositories/ppp.repository.ts
+++ src/modules/ppp/domain/repositories/ppp.repository.ts
@@ -2,7 +2,7 @@ import { PPPEntity } from '../../data/entities'
 
 export abstract class PPPRepository {
   
-  abstract findOnebyId(id: string): Promise<PPPEntity | null>
+  abstract findOnebyId(id: string): Promise< PPPEntity >
   abstract save(ppp: PPPEntity): Promise<PPPEntity>
   abstract findLastOneWithCompanyByStudent(studentCode: string): Promise< PPPEntity | null >
 
