d22faf41cc5bad65ae8d73a9e62466509792e3b2
Madai Mireli <madaichuco@hotmail.com>
2023-08-08 22:04:09 -0500

    update intershipHours

---
+++ src/modules/ppp/domain/use-cases/update-intership-hour.usecase.ts
@@ -0,0 +1,49 @@
+import { Response } from "express";
+
+import { CODE_STATUS } from "../../../../common/responses/code/code-status.ok";
+import { message } from "../../../../common/responses/msg.response";
+import { PPPRepository } from "../repositories";
+
+export class UpdateIntershipHourUseCase{
+
+    constructor(
+        private readonly response           : Response,
+        private readonly repository         : PPPRepository,
+        private readonly pppId              : string,
+        private readonly intershipHours     : number
+
+    ) {}
+
+    async execute() {
+
+        try {
+            const ppp = await this.repository.findOnebyId(this.pppId);
+
+            if(!ppp){
+                return message({
+                    response: this.response,
+                    code: CODE_STATUS.NOT_FOUND,
+                    info: `No se encontro PPP con id #${this.pppId}.`,
+                });
+            }
+
+            ppp.intershipHours = this.intershipHours;
+
+            this.repository.save(ppp)
+
+            message({
+                response: this.response,
+                code: CODE_STATUS.OK,
+                info: `Horas de prácticas actualizadas correctamente.`,
+            });
+
+        } catch (error) {
+
+            message({
+                response: this.response,
+                code: CODE_STATUS.INTERNAL_SERVER_ERROR,
+                info: error,
+            });
+        }
+    }
+}
