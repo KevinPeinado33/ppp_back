c703c019934928a6d58174b2aa811a4b943b60af
KevinPeinado33 <kevinpeinado@upeu.edu.pe>
2023-09-21 11:48:46 -0500

    tratando de arreglarla gaaaa

--- dist/src/modules/ppp/data/entities/question-answer.entity.js
+++ dist/src/modules/ppp/data/entities/question-answer.entity.js
@@ -23,7 +23,7 @@ __decorate([
     __metadata("design:type", String)
 ], QuestionAnswerEntity.prototype, "question", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ type: 'varchar' }),
+    (0, typeorm_1.Column)({ type: 'varchar', nullable: true }),
     __metadata("design:type", String)
 ], QuestionAnswerEntity.prototype, "answer", void 0);
 __decorate([
